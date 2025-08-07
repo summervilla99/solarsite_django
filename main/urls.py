2e20ff4eb5f9d3e8a8e9b8564491e947117a13fb
jihwan <jihwan99@icloud.com>
2025-08-07 14:55:31 +0900

    지도 검색 기능 구현

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import vworld_geocode
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("api/geocode/", vworld_geocode, name="geocode"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import JsonResponse
+import requests
 
 def index(request):
-    context = {
-        'name': '지환',
-        'users': [
-            {'name': '필준'},
-            {'name': '지민'},
-            {'name': '혁태'},
-        ]
-    }
+    return render(request, 'main/index.html', {
+        'vworld_key': settings.VWORLD_API_KEY
+    })
 
-    return render(request, 'index.html', context)
 
-def simple_map(request):
-    return render(request, 'index.html')
-
-# def map_view(request):
-#     land_data = LandCategory.objects.all()[:100]  # 성능 위해 100개만
-#     locations = []
-
-#     for land in land_data:
-#         if land.geom:
-#             locations.append({
-#                 'jibun': land.jibun,
-#                 'region': land.region,
-#                 'lat': land.geom.y,   # POINT(x y)에서 y=위도, x=경도
-#                 'lng': land.geom.x,
-#             })
-
-#     context = {'locations': locations}
-#     return render(request, 'map.html', context)
+def vworld_geocode(request):
+    address = request.GET.get("address")
+    for addr_type in ["road", "parcel"]:
+        res = requests.get(
+            "https://api.vworld.kr/req/address",
+            params={
+                "service": "address",
+                "request": "getcoord",
+                "format": "json",
+                "type": addr_type,
+                "key": settings.VWORLD_API_KEY,
+                "address": address,
+            },
+            timeout=5
+        )
+        data = res.json()
+        if data["response"]["status"] == "OK":
+            return JsonResponse(data)
+    return JsonResponse({"response": {"status": "NOT_FOUND"}}, status=404)
